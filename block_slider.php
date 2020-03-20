62c361e4598766efc5b05899f94e483476914330
Le Manh Hai <hai.lemanh.1310@gmail.com>
2020-03-20 21:55:30 +0700

    anh doc mobile

--- block_slider.php
+++ block_slider.php
@@ -4,14 +4,18 @@
         $data = array(
             array(
                 "src" => "images/slider.png",
+                "src1" => "images/anhdoc/Anh DOC 4.jpg",
             ),
             array(
                 "src" => "images/slider.png",
+                "src1" => "images/anhdoc/Anh DOC 4.jpg",
             ),
         );
         foreach ($data as $k => $v): ?>
         <li>
-            <img src="<?= $v['src'] ?>" alt="" uk-cover>
+            <img class="uk-visible@s" src="<?= $v['src'] ?>" alt="" uk-cover>
+            <img class="uk-hidden@s" src="<?= $v['src1'] ?>" alt="" uk-cover>
+            <div class="uk-position-cover uk-hidden@s uk-overlay uk-overlay-primary"></div>
             <div class="uk-position-center uk-position-small uk-text-center uk-light box2_lapdat" uk-scrollspy="target: > *; cls: uk-animation-slide-bottom; delay: 100">
                 <p class="uk-margin-remove">FPT - Thế giới tươi đẹp mở ra trước mắt bạn</p>
                 <div class="uk-margin"><a href="" class="uk-button uk-button-default uk-button-large uk-border-pill">Đăng ký ngay</a></div>
